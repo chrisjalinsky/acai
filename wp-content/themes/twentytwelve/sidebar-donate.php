b95871cf6f34a3d0bab13b2b81a39ce7f8936199
Chris Jalinsky <chris@altitudedigitalpartners.com>
2013-06-04 22:26:17 -0600

    Fixes #1. added sidebar donate and paypal.com link

---
+++ wp-content/themes/twentytwelve/sidebar-donate.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The sidebar containing the front page widget areas.
+ *
+ * If no active widgets in either sidebar, they will be hidden completely.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+/*
+ * The front page widget area is triggered if any of the areas
+ * have widgets. So let's check that first.
+ *
+ * If none of the sidebars have widgets, then let's bail early.
+ */
+//if ( ! is_active_sidebar( 'sidebar-2' ) && ! is_active_sidebar( 'sidebar-3' ) )
+	//return;
+
+// If we get this far, we have widgets. Let do this.
+?>
+<div id="secondary" class="widget-area" role="complementary">
+	<div class="base front-widgets">
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="ZXJU7YJ3KWXTG">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</form>
+
+	</div>
+	<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+	<div class="first front-widgets">
+		<?php dynamic_sidebar( 'sidebar-2' ); ?>
+	</div><!-- .first -->
+	<?php endif; ?>
+
+	<?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
+	<div class="second front-widgets">
+		<?php dynamic_sidebar( 'sidebar-3' ); ?>
+	</div><!-- .second -->
+	<?php endif; ?>
+</div><!-- #secondary -->
